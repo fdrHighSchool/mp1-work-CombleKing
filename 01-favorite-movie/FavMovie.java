e6c51b6656551cde0b97da3ce2af88fc9d389471
CombleKing <104275342+CombleKing@users.noreply.github.com>
2022-10-04 11:36:02 -0400

    Add files via upload

---
+++ 01-favorite-movie/FavMovie.java
@@ -0,0 +1,17 @@
+
+/**
+ * A description of a favorite movie.
+ *
+ * Yuxin Li
+ * 9/22/22
+ */
+public class FavMovie
+{
+  public static void main(String[] args) {
+    System.out.println("My favarite movie I watched over the summer is Bungo Stray Dogs - Dead Apple.");
+    System.out.println("The context to the movie is that there is a special group of people in the work known as Gifted. They have special powers that corralate with real life authors like Edger Allen Poe's ability is Black Cat in the Rue Mongue. Where the person can transport anyone into any murder mystery novel.");
+    System.out.println("The movie is about a Gifted who creates a smokescreen across Yokohama, where normal people dissapear and gifted fight against their own abilities.");
+    System.out.println("Our Main character, Atsushi Nakajima, and his freinds has to fight against their abilities and Atsushi must overcome his childhood trauma to save his senpai Dazai from the villain engulfing Yokohoma.");
+    System.out.println("I highly suggest you watch this movie in the free time as it was a real tear jerker at the end of the movie.");
+  }
+}
