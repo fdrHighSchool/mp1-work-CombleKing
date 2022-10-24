b9bcb2c6f3a7361a2b887c5d14dd7f1ba0345140
Li Fiona <lifiona@lis-macbook-pro.78b505.nycboe.org>
2022-10-24 12:06:29 -0400

    update

--- 06-username/username.java
+++ 06-username/username.java
@@ -4,8 +4,8 @@ public class username {
   public static void main(String[] args) {
     // create Scanner object
     Scanner s = new Scanner(System.in);
-    // get String input from user
 
+    
     System.out.print("Are you a teacher or student?");
     String role = s.nextLine();
     System.out.print("Enter your first name: ");
@@ -16,6 +16,8 @@ public class username {
     lastName =lastName.toLowerCase();
     System.out.print("Enter your favorite number: ");
     int number = s.nextInt();
+
+    System.out.println(genpass());
 
 
   if (role.equalsIgnoreCase("student")){
@@ -46,4 +48,51 @@ public class username {
     return n.substring(0, 1);
   } // end initialize method
 
+  public static String genpass() {
+    String pass = "";
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter you password length.");
+    int length = s.nextInt();
+  
+      for (int i = 0; i<length;i++){
+        
+        int cap = (int)(Math.random()*(90-64+1)+64);
+        int lower = (int)(Math.random()*(122 -97 +1) +97);
+        int num = (int)(Math.random()*(57-33+1) + 33);
+  
+  
+        char upper = (char)cap;
+        char lowerc = (char)lower;
+        char numberspec = (char)num;
+       
+  
+        pass += lowerc;
+        pass += upper;
+        pass += numberspec;
+      
+          
+  
+      
+  }
+
+
+  pass =pass.substring(0,length);
+  s.close();
+  return pass;
+  }
+
+
+
+
 } // end class
+
+
+//import a scannner
+///process: 1) random # 65-90 cap letter
+//97-122 lower case
+//33-47 special symbols
+//have # conver to character via c= (char)i
+//add to a string and retunr the password
+
+
+
